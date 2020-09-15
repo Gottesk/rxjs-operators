fa4c472a2634a138afefbf5aeb29783788724600
Dmitriy Lopukhov <87dl@mail.ru>
2020-09-15 16:43:18 +0200

    Create cache-locally.operator.ts

---
+++ cache-locally.operator.ts
@@ -0,0 +1,66 @@
+import { MonoTypeOperatorFunction, Observable, iif, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+const expirationTimeDefault = 60 * 5000;
+
+const storageKey = 'dataCache';
+
+interface ICachedItem<T> {
+    key: string;
+    cachedDate: Date;
+    data: T;
+}
+interface IDataCache<T> {
+    [key: string]: ICachedItem<T>;
+}
+
+const getDataCache = <T>() => {
+    const dataCache: IDataCache<T> = JSON.parse(localStorage.getItem(storageKey)) || {};
+    return dataCache;
+};
+
+const getItem = <T>(key: string): ICachedItem<T> => {
+    const dataCache: IDataCache<T> = getDataCache();
+    return dataCache[key];
+};
+
+const setItem = <T>(key: string, data: T) => {
+    const dataCache: IDataCache<T> = getDataCache();
+    const item: ICachedItem<T> = {
+        key,
+        cachedDate: new Date(),
+        data
+    };
+    const newDataCache: IDataCache<T> = { ...dataCache, [key]: item };
+    localStorage.setItem(storageKey, JSON.stringify(newDataCache));
+};
+
+const isCacheAvailable = <T>(key: string, expirationTime: number): boolean => {
+    const item = getItem<T>(key);
+    if (!item) {
+        return false;
+    }
+    const nowDate = new Date();
+    const cachedDate = new Date(item.cachedDate);
+    const expirationDate = new Date(cachedDate.getTime() + expirationTime);
+    return expirationDate > nowDate;
+};
+
+const getFromCache = <T>(key: string): T => {
+    const item = getItem<T>(key);
+    return item && item.data;
+};
+
+export function cacheLocally<T>(key: string, request: Observable<T>, expirationTime = expirationTimeDefault): MonoTypeOperatorFunction<T> {
+    return input$ => input$.pipe(
+        () =>
+            iif(() => isCacheAvailable(key, expirationTime),
+                of(getFromCache(key)),
+                request.pipe(
+                    tap(data => {
+                        setItem(key, data);
+                    })
+                ),
+            )
+    );
+}

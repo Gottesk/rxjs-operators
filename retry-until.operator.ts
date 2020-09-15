a912b43fca8d016cd5176ea44eaa3b8cf6b884d0
Dmitriy Lopukhov <87dl@mail.ru>
2020-09-15 20:04:40 +0200

    Update retry-until.operator.ts

--- retry-until.operator.ts
+++ retry-until.operator.ts
@@ -1,6 +1,7 @@
 import { Observable, interval, MonoTypeOperatorFunction } from 'rxjs';
 import { retryWhen, takeUntil, filter } from 'rxjs/operators';
 
+/** Observable will be triggered with an interval until the conditions is met. */
 export const retryUntil = <T>(condition: Observable<T>, delayMilliseconds: number = 3000): MonoTypeOperatorFunction<T> =>
     input$ => input$.pipe(
         retryWhen(() => interval(delayMilliseconds)),
